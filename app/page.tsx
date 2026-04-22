d2b3659a3dc51ecbabc47c3e797a07dce5dd3faa
ServingTHEvibe <servingthevibe@gmail.com>
2026-04-21 21:04:29 -0500

    Update page: add PromoBanner, About, SocialProof sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import PromoBanner from '@/components/PromoBanner'
 import Nav from '@/components/Nav'
 import Hero from '@/components/Hero'
 import ScrollingCan from '@/components/ScrollingCan'
 import Manifesto from '@/components/Manifesto'
+import About from '@/components/About'
+import SocialProof from '@/components/SocialProof'
 import ProductSpotlight from '@/components/ProductSpotlight'
 import Flavors from '@/components/Flavors'
 import Merch from '@/components/Merch'
@@ -11,15 +14,18 @@ import Footer from '@/components/Footer'
 export default function Home() {
   return (
     <main className="bg-black">
+      <PromoBanner />
       <Nav />
       <ScrollingCan />
       <Hero />
       <Manifesto />
+      <About />
+      <SocialProof />
       <ProductSpotlight />
       <Flavors />
       <Merch />
       <StoreLocator />
       <Footer />
     </main>
-  );
+  )
 }
